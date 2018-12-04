99bad3c2fac5e52aad6dd8600a8036bd9e9f0ce4
sczhaoqi <1151324240@qq.com>
2018-12-05 00:40:21 +0800

    Init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import NavCom from './layouts/NavCom'
+import MainCom from './layouts/MainCom'
+import LeftCom from './layouts/LeftCom'
+import Child4Com from './layouts/Child4Com'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  constructor(props){
+    super(props)
+    this.state={
+    isMain: true,
+    childBg:'pink',
+    navCom: <NavCom />,
+    leftCom:<LeftCom />,
+    mainCom: <MainCom/>
+    }
+    this.changeCom= this.changeCom.bind(this)
+    }
+   render(){
+   // 父组件通过state
+    return(
+    <div>
+    <div className="nav">
+    {this.state.navCom}
+    </div>
+    <div className="left">
+    {this.state.leftCom}
+    </div>
+    <div className="main">
+    {this.state.mainCom}
+    </div>
+    <button onClick={this.changeCom}>更改组件</button>
+    </div>
+    )
+   }
+ 
+   changeCom(){
+    if(this.state.isMain){
+      this.setState({
+        mainCom:<Child4Com/>,
+        isMain: false
+        })
+    }else{
+      this.setState({
+        mainCom:<MainCom/>,
+        isMain: true
+        })
+    }
+   }  
 }
 
 export default App;
