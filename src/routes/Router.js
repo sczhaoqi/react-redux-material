d13857f08b18db834253299f291a4228d99efa85
sczhaoqi <1151324240@qq.com>
2018-12-06 00:47:39 +0800

    error useage of function components

---
+++ src/routes/Router.js
@@ -0,0 +1,11 @@
+import Home from 'components/home/Home';
+import SignIn from 'components/login/SignIn';
+
+
+const BasicRoute = [
+    { path: "/", component: Home},
+    { path: "/signin", component: SignIn}
+];
+
+
+export default BasicRoute;
